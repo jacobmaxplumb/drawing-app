8c02515c2658d13eefb34a994faa90be2c5c4ac5
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-09-30 20:42:46 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { ColorPicker } from './components/ColorPicker/ColorPicker';
 import { Container } from '@mui/material';
 import { useState } from 'react';
+import { Canvas } from './components/Canvas/Canvas';
 
 function App() {
   const [color, setColor] = useState('');
@@ -13,6 +14,7 @@ function App() {
   return (
     <Container maxWidth="lg">
       <ColorPicker color={color} handleColorChange={handleColorChange} />
+      <Canvas />
     </Container>
   );
 }
---
+++ src/components/Canvas/Canvas.js
@@ -0,0 +1,37 @@
+import { Paper } from "@mui/material";
+import './Canvas.css';
+import React from "react";
+
+export class Canvas extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            dots: []
+        }
+    }
+
+    addDot = (e) => {
+        const styles = {
+            width: '10px', 
+            height: '10px', 
+            backgroundColor: 'blue', 
+            position: 'absolute', 
+            top: e.clientY, 
+            left: e.clientX
+        }
+        const dot = <div style={styles}></div>
+        this.setState({dots: [...this.state.dots, dot]});
+    }
+
+    render() {
+        return (
+            <Paper onClick={this.addDot} className="canvas" elevation={3}>
+                {this.state.dots.map((dot, i) => (
+                    <div key={i}>
+                        {dot}
+                    </div>
+                ))}
+            </Paper>
+        );
+    }
+}
